dd2268885b6f7de21a3cb91a0c98cb5de8f15f84
marcottemickyncrea <marcotte.mickael@isen-ouest.yncrea.fr>
2022-12-15 10:02:35 +0100

    route GET & POST

--- airbnb/app/routes.py
+++ airbnb/app/routes.py
@@ -1,56 +1,39 @@
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, request, redirect, url_for, flash
 from app import app
 
-@app.route('/')
-def index():
-    posts = [
-        {
-            'url': 'https://a0.muscache.com/im/pictures/d72d00a9-cb72-445b-92c7-285d31b3e794.jpg?im_w=720',
-            'adresse': 'Quiberville, France',
-            'type':'Particulier',
-            'date': '8-13 janv.',
-            'prix': '194',
-            'note': '4.71',
-        },
-        {
-            'url': 'https://a0.muscache.com/im/pictures/aa5dcdd3-9b49-497f-aa4e-114414dd419e.jpg?im_w=720',
-            'adresse': 'Criel-sur-Mer, France',
-            'type':'Particulier',
-            'date': '10-16 dec.',
-            'prix': '149',
-            'note': '4.74',
-        },
-        {
-            'url': 'https://a0.muscache.com/im/pictures/miso/Hosting-631888526880090604/original/2c848fcd-7fb0-4533-bb0e-a6970487afc8.jpeg?im_w=720',
-            'adresse': 'Criel-sur-Mer, France',
-            'type':'Particulier',
-            'date': '10-16 dec.',
-            'prix': '274',
-            'note': '4.79',
-        },
-        {
-            'url': 'https://a0.muscache.com/im/pictures/0a67e415-dd30-4724-9dc0-8bf3fb0bb05a.jpg?im_w=720',
-            'adresse': 'Dieppe, France',
-            'type':'Particulier',
-            'date': '2-7 janv.',
-            'prix': '86',
-            'note': '4.89',
-        },
-        {
-            'url': 'https://a0.muscache.com/im/pictures/d20f5257-d4df-45c7-b816-c39e15a9961c.jpg?im_w=720',
-            'adresse': 'Le tréport, France',
-            'type':'Particulier',
-            'date': '9-14 dec.',
-            'prix': '172',
-            'note': '4.67',
-        },
-        {
-            'url': 'https://a0.muscache.com/im/pictures/0f1fc62e-8fb9-4b6d-a6d4-f3021c13dbe3.jpg?im_w=720',
-            'adresse': 'Dieppe, France',
-            'type':'Professionnel',
-            'date': '10-16 dec.',
-            'prix': '92',
-            'note': '4.74',
-        },
-    ]
-    return render_template('index.html', title='Home', posts=posts)
+import mysql.connector as mysqlpy
+
+user = 'root'
+password = 'example'
+host = 'localhost'
+port = '3308'
+database = 'airbnb'
+
+bdd = mysqlpy.connect(user=user, password=password,
+                      host=host, port=port, database=database)
+cursor = bdd.cursor()
+
+@app.route('/index')
+def destinations():
+    cursor.execute('SELECT * FROM destinations;')
+    destinations = cursor.fetchall()
+    return render_template('index.html', destinations = destinations)
+
+@app.route('/ajout-destination', methods= ['GET', 'POST'])
+def ajouter_destination():
+    if request.method == 'POST':
+        url_image = request.form['url-image']        
+        adresse_destination = request.form['localisation']
+        type_destination = request.form['type']
+        dates = request.form['dates']
+        prix_destination = request.form['prix']
+        note_destination = 0
+        query=('INSERT INTO destinations(url_image, adresse_destination, type_destination, dates, prix_destination, note_destination) VALUES(%s,%s,%s,%s,%s,%s);')
+        cursor.execute(query, (url_image, adresse_destination, type_destination, dates, prix_destination, note_destination))
+        bdd.commit() 
+        return redirect('index')
+    return render_template('ajout-destination.html')
+
+@app.route('/modifier-destination', methods = ['GET', 'POST'])
+def modifier_destination():
+    return render_template('modifier-destination.html')
